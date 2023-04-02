40bbd54d574fe0bdc7d8be0a5254afb4dfcb5074
AlphaKeks <alphakeks@dawn.sh>
2023-04-02 14:39:15 +0200

    formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,8 @@
--- Install packer (our plugin manager) automatically if it's not already
--- installed. It will clone the repo as soon as you open neovim. Then re-open
--- neovim and you should have access to various `:Packer` commands.
--- The first one of those that you should run is `:PackerSync` to install all
--- your plugins.
+-- Install packer (our plugin manager) automatically if it's not
+-- already installed. It will clone the repo as soon as you open
+-- neovim. Then re-open neovim and you should have access to various
+-- `:Packer` commands. The first one of those that you should run is
+-- `:PackerSync` to install all your plugins.
 local packer_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
 	vim.fn.system({
@@ -35,21 +35,23 @@ require("packer").startup(function(use)
 	-- Treesitter abstraction to easily configure it
 	use("nvim-treesitter/nvim-treesitter")
 
-	-- preset LSP configurations + easier to use API
-	-- read this for all available language servers:
+	-- preset LSP configurations + easier to use API read this for
+	-- all available language servers:
 	--   https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	use("neovim/nvim-lspconfig")
 
-	-- completion enginge that can pickup from language servers and other sources
+	-- completion enginge that can pickup from language servers and
+	-- other sources
 	use({
 		"hrsh7th/nvim-cmp",
 		requires = {
-			-- nvim-cmp requires a snippet enginge to work, even if you're not gonna use snippets.
+			-- nvim-cmp requires a snippet enginge to work, even if
+			-- you're not gonna use snippets.
 			"L3MON4D3/LuaSnip",
 
 			-- These are called "sources" -- they are feeding
-			-- autocompletion results into nvim-cmp, which will show them
-			-- on your screen.
+			-- autocompletion results into nvim-cmp, which will show
+			-- them on your screen.
 
 			"hrsh7th/cmp-buffer", -- This will grab words from your buffer
 			"hrsh7th/cmp-nvim-lsp", -- This will grab suggestions from your language server.
@@ -59,9 +61,9 @@ end)
 
 --[[ setup plugins HERE ]]--
 
--- `pcall` returns a boolean + the return value of the function you pass to it.
--- The boolean determines whether the function call succeeded or not without
--- crashing your program if it fails.
+-- `pcall` returns a boolean + the return value of the function you
+-- pass to it. The boolean determines whether the function call
+-- succeeded or not without crashing your program if it fails.
 local catppuccin_installed, catppuccin = pcall(require, "catppuccin")
 if catppuccin_installed then
 	-- load the default configuration
@@ -76,7 +78,7 @@ local treesitter_installed, treesitter = pcall(require, "nvim-treesitter.configs
 if treesitter_installed then
 	treesitter.setup({
 		-- Everytime you launch nvim treesitter will make sure these
-		-- servers are installed and up to date.
+		-- parsers are installed and up to date.
 		ensure_installed = {
 			"help",
 			"vim",
@@ -85,8 +87,8 @@ if treesitter_installed then
 		},
 
 		-- Use treesitter for indentation as well. This is known to
-		-- cause problems in some languages (e.g. python) so you might
-		-- need to disable it for certain filetypes.
+		-- cause problems in some languages (e.g. python) so you
+		-- might need to disable it for certain filetypes.
 		-- Docs: https://github.com/nvim-treesitter/nvim-treesitter#indentation
 		ident = {
 			enable = true
@@ -132,19 +134,19 @@ if lspconfig_installed then
 		-- will be.
 		vim.keymap.set("n", "gl", vim.diagnostic.open_float)
 
-		-- This will jump your cursor to the next available diagnostic
-		-- and display it.
+		-- This will jump your cursor to the next available
+		-- diagnostic and display it.
 		vim.keymap.set("n", "gL", vim.diagnostic.goto_next)
 	end
 
-	-- The diagnostic API is not exclusive to LSP but heavily used by it.
-	-- If you want it to look fancy, you should configure it :)
+	-- The diagnostic API is not exclusive to LSP but heavily used by
+	-- it. If you want it to look fancy, you should configure it :)
 	-- :h vim.diagnostic.config
 	vim.diagnostic.config({
 		-- Will display errors on the line they occur in.
 		virtual_text = {
-			-- Will show which language server/source the
-			-- diagnostic message is coming from.
+			-- Will show which language server/source the diagnostic
+			-- message is coming from.
 			source = true,
 
 			-- This is a custom prefix that I personally like to
@@ -153,8 +155,8 @@ if lspconfig_installed then
 			prefix = "",
 
 			-- This will only show error messages, not warnings or
-			-- anything. You can still read them with the `gl`
-			-- keymap from above, they just won't show inline.
+			-- anything. You can still read them with the `gl` keymap
+			-- from above, they just won't show inline.
 			severity = vim.diagnostic.severity.ERROR
 		},
 
@@ -168,10 +170,10 @@ if lspconfig_installed then
 
 		-- The float window from `gl`
 		float = {
-			-- This will enable you to press `gl` again and put
-			-- your cursor inside the window so you can move around
-			-- and copy stuff. You can go back with `<esc>`,
-			-- `<C-o>` or `q`.
+			-- This will enable you to press `gl` again and put your
+			-- cursor inside the window so you can move around and
+			-- copy stuff. You can go back with `<esc>`, `<C-o>` or
+			-- `q`.
 			focusable = true,
 
 			-- Always show which diagnostic message come from which
@@ -190,9 +192,9 @@ if lspconfig_installed then
 	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 
-	-- This section will make more sense once we move onto `nvim-cmp` but
-	-- just know that this is necessary to get stuff like auto-imports when
-	-- using autocompletion.
+	-- This section will make more sense once we move onto `nvim-cmp`
+	-- but just know that this is necessary to get stuff like
+	-- auto-imports when using autocompletion.
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	local cmp_installed, cmp_lsp = pcall(require, "cmp_nvim_lsp")
 	if cmp_installed then
@@ -219,7 +221,8 @@ if lspconfig_installed then
 
 	-- ^ This is the same for every language server. Read the
 	-- `server_configurations.md` I linked earlier to find out how to
-	-- install and configure them; but it's the same pattern every time.
+	-- install and configure them; but it's the same pattern every
+	-- time.
 end
 
 local cmp_installed, cmp = pcall(require, "cmp")
@@ -260,10 +263,10 @@ if cmp_installed and luasnip_installed then
 		sources = {
 			{ name = "nvim_lsp" },
 
-			-- We only want 1 buffer word to be shown at a time,
-			-- feel free to change this of course.
-			-- `keyword_length` will determine how many characters
-			-- you need to type before a word gets suggested.
+			-- We only want 1 buffer word to be shown at a time, feel
+			-- free to change this of course. `keyword_length` will
+			-- determine how many characters you need to type before
+			-- a word gets suggested.
 			{ name = "buffer", max_item_count = 1, keyword_length = 5 }
 		},
 		formatting = {
@@ -271,13 +274,12 @@ if cmp_installed and luasnip_installed then
 			format = function(_, item)
 				item.menu = ""
 
-				-- If you have a font with icon support
-				-- installed you can use these, they will show
-				-- up in your completion menu based on the kind
-				-- of item you get suggested. If you don't have
-				-- such a font or just simply prefer words,
-				-- just remove this line and it will use words
-				-- instead.
+				-- If you have a font with icon support installed you
+				-- can use these, they will show up in your
+				-- completion menu based on the kind of item you get
+				-- suggested. If you don't have such a font or just
+				-- simply prefer words, just remove this line and it
+				-- will use words instead.
 				item.kind = ({
 					Text = "", Method = "", Function = "", Constructor = "",
 					Field = "", Variable = "", Class = "", Interface = "",
